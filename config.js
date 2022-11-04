5eaefd517236821b09dbc7f6e5590a6ae7a164ac
ZGQ <73878474+ZGQ-inc@users.noreply.github.com>
2022-11-04 17:26:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1884169-f2ddd4677953551305552673',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '个人主页',
+      url: 'https://zgq-inc.github.io/'
+    },
+    {
+      text: '项目地址',
+      url: 'https://github.com/ZGQ-inc/uptime-status'
+    },
+    {
+      text: '原项目地址',
+      url: 'https://github.com/yb/uptime-status'
+    },
+  ],
+};
